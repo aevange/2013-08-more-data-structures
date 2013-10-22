1e5fdcb996d6b1b51d31805e5c8a89863bcd0fcc
aevange <andre.evangelesta@gmail.com>
2013-10-21 23:53:39 -0700

    Redo binarySearchTree for practice.

--- spec/binarySearchTreeSpec.js
+++ spec/binarySearchTreeSpec.js
@@ -2,7 +2,7 @@ describe("binarySearchTree", function() {
   var binarySearchTree;
 
   beforeEach(function() {
-    binarySearchTree = new makeBinarySearchTree(6);
+    binarySearchTree = makeBinarySearchTree(6);
   });
 
   it("should have methods named 'insert', 'contains', and 'depthFirstLog", function() {
@@ -11,4 +11,40 @@ describe("binarySearchTree", function() {
     expect(binarySearchTree.depthFirstLog).toEqual(jasmine.any(Function));
   });
   // add more tests here to test the functionality of binarySearchTree
+
+  it("should set the first value passed", function(){
+    expect(binarySearchTree.value).toEqual(6);
+  });
+
+
+  it("should inset a lesser value to the left", function(){
+    binarySearchTree.insert(4);
+    expect(binarySearchTree.left.value).toEqual(4);
+    binarySearchTree.insert(2);
+    expect(binarySearchTree.left.left.value).toEqual(2);
+  });
+
+  it("should inset a greater value to the right", function(){
+    binarySearchTree.insert(10);
+    expect(binarySearchTree.right.value).toEqual(10);
+    binarySearchTree.insert(100);
+    expect(binarySearchTree.right.right.value).toEqual(100);
+  });
+  it("should know where to insert values", function(){
+    binarySearchTree.insert(4);
+    expect(binarySearchTree.left.value).toEqual(4);
+    binarySearchTree.insert(5);
+    expect(binarySearchTree.left.right.value).toEqual(5);
+  });
+
+  it("should find a value that has been placed within the tree.", function(){
+    expect(binarySearchTree.contains(6)).toEqual(true);
+    binarySearchTree.insert(3);
+    expect(binarySearchTree.contains(6)).toEqual(true);
+    expect(binarySearchTree.contains(3)).toEqual(true);
+    binarySearchTree.insert(4);
+    binarySearchTree.insert(8);
+    expect(binarySearchTree.contains(8)).toEqual(true);
+    expect(binarySearchTree.contains(10)).toEqual(false);
+  });
 });
--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,28 +1,62 @@
-var makeBinarySearchTree = function(val){
-  var tree;
+var makeBinarySearchTree = function(val) {
+  var tree = {};
+  tree.value = val;
+  tree.left = null;
+  tree.right = null;
+
+  extend(tree, binaryMethods);
+
   return tree;
 };
 
-makeBinarySearchTree.prototype.insert = function(input){
-  if(input < this.value){
-    if(this.left === undefined){
+var extend = function(originalRecipe, extraCrispy) {
+  for(var meth in extraCrispy){
+    originalRecipe[meth] = extraCrispy[meth];
+  }
+};
+
+var binaryMethods = {};
+
+binaryMethods.insert = function(val) {
+  if(val < this.value) {
+    if(this.left === null) {
       this.left = makeBinarySearchTree(val);
+    } else {
+      this.left.insert(val);
+    }
+  } else if (val > this.value) {
+    if(this.right === null) {
+      this.right = makeBinarySearchTree(val);
+    } else {
+      this.right.insert(val);
     }
   }
 };
 
-makeBinarySearchTree.prototype.contains = function(){
-
+binaryMethods.contains = function(val) {
+  var value = this.value;
+  var found = val === value;
+  if(!found) {
+    if (val < value) {
+      if(this.left !== null) {
+        found = this.left.contains(val);
+      }
+    } else if (val > value) {
+      if (this.right !== null) {
+        found = this.right.contains(val);
+      }
+    }
+  }
+  return found;
 };
 
-makeBinarySearchTree.prototype.depthFirstLog = function(){
-
+binaryMethods.depthFirstLog = function(callback) {
+  callback.call(this);
+  this.left && this.left.depthFirstLog(callback);
+  this.right && this.right.depthFirstLog(callback);
 };
 
-var makeNode = function(val){
-  var node = {};
-  node.value = val;
-  node.right;
-  node.left;
-  return node;
-};
+// binaryMethods.bredthFirstLog() = function(callback) {
+//   var generations = [];
+//   generations.push([this]);
+// }
