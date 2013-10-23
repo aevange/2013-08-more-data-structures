7298098a809f952745176caa9db517876ff0c2cd
aevange <andre.evangelesta@gmail.com>
2013-10-22 19:58:35 -0700

    Remove console.log

--- src/hashTable.js
+++ src/hashTable.js
@@ -5,7 +5,6 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  console.log(i);
   var values = this._storage.get(i) || [];
   values.push([k,v]);
   this._storage.set(i, values);
@@ -15,7 +14,6 @@ HashTable.prototype.retrieve = function(k){
   var value;
   var i = getIndexBelowMaxForKey(k, this._limit);
   var elements = this._storage.get(i);
-  console.log(elements);
   _.each(elements, function(el){
     el[0] === k && (value = el[1]);
   });
