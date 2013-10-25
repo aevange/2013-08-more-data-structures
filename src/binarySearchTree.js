b2b5052294266834184fbf5d0f86737b7052bfc2
aevange <andre.evangelesta@gmail.com>
2013-10-24 23:15:23 -0700

    Add annotation for bredthFirstLog method

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -63,18 +63,43 @@ binaryMethods.bredthFirstLog = function(callback) {
   }
 };
 
+// Returns an array that will acts as a queue.
+// Starting wiht which ever node calls the method, the array
+// is populated bredth first from "left" to "right" in
+// accordance to the Binary Tree setup
 binaryMethods.makeQueue = function() {
-  var queue = [this];
-  var descendents = getDescendent([this]);
-  return queue.concat(descendents);
-}
 
+  // initializes the queue with the node which called the method
+  var queue = [this];
+
+  // all the following descendants of the caller node in an array
+  // sorted by bredth
+
+  //the DESCENDENTS are a punk band from Manhattan Beach, California
+  //http://en.wikipedia.org/wiki/Descendents
+  var descendents = getDescendent([this]);
+  //Suburban Home from the album Milo Goes to College is a pretty good song
+  return queue.concat(descendents);
+};
+
+// Recursive function which populates an array one "level"
+// of a binary tree at a time from a given array of nodes
+
+//Hope is another good song from Milo Goes to Collge
 var getDescendent = function(array) {
   var results = [];
+  // populates the results array with any descendants of the nodes in argument array
+
+  //Sublime has done a great cover of Hope on 40oz to Freedom
   for (var i = 0; i < array.length; i++) {
     array[i].left && results.push(array[i].left);
     array[i].right && results.push(array[i].right);
   }
+  // if any desendants of the nodes from the argument array have been found
+  // recursivly call the function, passing in the newly found nodes as the argument
+
+  //Sublime is a ska-punk band from Long Beach, California
+  //http://en.wikipedia.org/wiki/Sublime_(band)
   if (results.length) {
     results = results.concat(getDescendent(results));
   }
