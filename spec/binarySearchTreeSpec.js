a3cc2be1be8c44520323ba6bcc5fe69db7abc6b2
aevange <andre.evangelesta@gmail.com>
2013-10-25 22:11:37 -0700

    Add test for makeQueue method

--- spec/binarySearchTreeSpec.js
+++ spec/binarySearchTreeSpec.js
@@ -5,11 +5,12 @@ describe("binarySearchTree", function() {
     binarySearchTree = makeBinarySearchTree(6);
   });
 
-  it("should have methods named 'insert', 'contains', 'depthFirstLog' and 'bredthFirstLog'", function() {
+  it("should have methods named 'insert', 'contains', 'depthFirstLog', 'bredthFirstLog' and 'makeQueue'", function() {
     expect(binarySearchTree.insert).toEqual(jasmine.any(Function));
     expect(binarySearchTree.contains).toEqual(jasmine.any(Function));
     expect(binarySearchTree.depthFirstLog).toEqual(jasmine.any(Function));
     expect(binarySearchTree.bredthFirstLog).toEqual(jasmine.any(Function));
+    expect(binarySearchTree.makeQueue).toEqual(jasmine.any(Function));
   });
 
   it("should set the first value passed", function(){
@@ -46,5 +47,33 @@ describe("binarySearchTree", function() {
     binarySearchTree.insert(8);
     expect(binarySearchTree.contains(8)).toEqual(true);
     expect(binarySearchTree.contains(10)).toEqual(false);
+  });
+
+  it("should create a queue of nodes, bredth first, from 'left' to 'right'", function(){
+    //gen 2
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(9);
+    //gen 3
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(8);
+    binarySearchTree.insert(10);
+    binarySearchTree.insert(4);
+    //gen 4
+    binarySearchTree.insert(5);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(11);
+    binarySearchTree.insert(1);
+    var queue = binarySearchTree.makeQueue();
+    expect(queue[0].value).toEqual(6);
+    expect(queue[1].value).toEqual(3);
+    expect(queue[2].value).toEqual(9);
+    expect(queue[3].value).toEqual(2);
+    expect(queue[4].value).toEqual(4);
+    expect(queue[5].value).toEqual(8);
+    expect(queue[6].value).toEqual(10);
+    expect(queue[7].value).toEqual(1);
+    expect(queue[8].value).toEqual(5);
+    expect(queue[9].value).toEqual(7);
+    expect(queue[10].value).toEqual(11);
   });
 });
